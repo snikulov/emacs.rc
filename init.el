09b00fad3f1e87fdf9bc454dc156e93cb57b4295
Sergey Nikulov <sergey.nikulov@gmail.com>
2010-08-28 03:05:19 +1100

    initial

---
+++ init.el
@@ -0,0 +1,18 @@
+;;;
+;;; Initialization of Emacs
+;;;
+
+;;; Initialize gtags
+(setq load-path (cons "/usr/share/gtags" load-path))
+(autoload 'gtags-mode "gtags" "" t)
+
+(setq c-mode-hook
+      '(lambda ()
+	      (gtags-mode 1)
+))
+
+(setq c++-mode-hook
+      '(lambda ()
+	      (gtags-mode 1)
+))
+
