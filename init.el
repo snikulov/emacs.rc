f49fbd7ca938954d6ec606e25a5eb08c5e829e05
Sergey Nikulov <sergey.nikulov@gmail.com>
2010-08-29 23:09:16 +1100

    formatting mostly

--- init.el
+++ init.el
@@ -2,19 +2,21 @@
 ;;; Initialization of Emacs
 ;;;
 
-;;; Initialize gtags
+;;;
+;;; disable scroll, tool, menu bars
+;;;
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;;;
+;;; Initialize gtags and modes
+;;;
 (setq load-path (cons "/usr/share/gtags" load-path))
 (autoload 'gtags-mode "gtags" "" t)
 
-(setq c-mode-hook
-      '(lambda ()
-	      (gtags-mode 1)
-))
-
-(setq c++-mode-hook
-      '(lambda ()
-	      (gtags-mode 1)
-))
+(setq c-mode-hook '(lambda () (gtags-mode 1)))
+(setq c++-mode-hook '(lambda () (gtags-mode 1)))
 
 ;;;
 ;;; set color theme
@@ -22,3 +24,16 @@
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-euphoria)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#00ff00" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
