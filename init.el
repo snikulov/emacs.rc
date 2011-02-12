3767ea30a4e97c27704cf0ba21fd4b4e2baf4a6d
Sergey Nikulov <sergey.nikulov@gmail.com>
2011-02-13 01:00:11 +1000

    small updates

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 ;; el-get
 (setenv "ERGOEMACS_KEYBOARD_LAYOUT" "dv")
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+
 (add-to-list 'load-path "~/emacs.rc/el-get/")
 (require 'el-get)
 (setq el-get-recipe-path  '("~/emacs.rc/el-get/recipes/"))
@@ -9,9 +15,20 @@
 
 (ergoemacs-mode 1)
 
+
+;; gtags on Fedora
+(setq load-path (cons "/usr/share/gtags" load-path))
+(autoload 'gtags-mode "gtags" "" t)
+
+(setq c-mode-hook '(lambda () (gtags-mode 1)))
+(setq c++-mode-hook '(lambda () (gtags-mode 1)))
+(setq c-mode-hook '(lambda () (global-linum-mode 1)))
+(setq c++-mode-hook '(lambda () (global-linum-mode 1)))
+
+
 (require 'color-theme)
 (setq color-theme-is-global t)
-(color-theme-renegade)
+(color-theme-euphoria)
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,3 +41,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "cyan3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+
+
+
+
