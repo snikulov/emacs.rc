45f9231f7ad7b46efbb2c2ba00f72e6e45630496
Sergey Nikulov <sergey.nikulov@gmail.com>
2011-02-12 23:23:03 +1000

    updated to ergoemacs dv layout

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
 ;; el-get
-
+(setenv "ERGOEMACS_KEYBOARD_LAYOUT" "dv")
 (add-to-list 'load-path "~/emacs.rc/el-get/")
 (require 'el-get)
 (setq el-get-recipe-path  '("~/emacs.rc/el-get/recipes/"))
-(setq el-get-sources '(magit psvn color-theme cmake-mode
-                       cedet ergoemacs-keybindings 
-                       ahg dvc ecb auto-complete
-                       planner remember yasnippet))
+(setq el-get-sources '(color-theme cmake-mode magit psvn
+    ergoemacs-keybindings ahg auto-complete yasnippet))
 (el-get 'sync)
+
+(ergoemacs-mode 1)
