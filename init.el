53e11b6a40e31349c28e6673a5c68b6f5afae0d0
Sergey Nikulov <sergey.nikulov@gmail.com>
2011-02-13 00:07:36 +1000

    increased font && added remember

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (require 'el-get)
 (setq el-get-recipe-path  '("~/emacs.rc/el-get/recipes/"))
 (setq el-get-sources '(color-theme cmake-mode magit psvn
-    ergoemacs-keybindings ahg auto-complete yasnippet))
+    ergoemacs-keybindings ahg auto-complete yasnippet remember))
 (el-get 'sync)
 
 (ergoemacs-mode 1)
@@ -23,4 +23,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "cyan3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 119 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "cyan3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
