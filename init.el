a73bdb76cf50088ecb4de07cb72d034ee1c8bd95
Sergey Nikulov <sergey.nikulov@gmail.com>
2011-02-12 23:43:55 +1000

    color theme && font

--- init.el
+++ init.el
@@ -8,3 +8,19 @@
 (el-get 'sync)
 
 (ergoemacs-mode 1)
+
+(require 'color-theme)
+(setq color-theme-is-global t)
+(color-theme-renegade)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "cyan3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 119 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
