9cf7092ab01cb616f15d8494343f1c354cdb723f
Sergey Nikulov <sergey.nikulov@gmail.com>
2011-04-19 02:09:29 +1100

    some updates in font-face

--- init.el
+++ init.el
@@ -51,18 +51,14 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 (color-theme-euphoria)
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "cyan3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+
+(set-face-attribute 'default nil :height 142)
+(global-set-key (kbd "<C-M-down>") 'text-scale-decrease)
+(global-set-key (kbd "<C-M-up>") 'text-scale-increase)
+
+(when (equal system-type 'gnu/linux)
+  (set-face-font 'default nil :family "Droid Sans Mono")
+)
 
 
 
